b74872ee05031694a97bfe9096cfa9a4cadc9776
Alex Bortnik <bortnik.ar@gmail.com>
2022-10-15 18:43:42 +0300

    Decrypt as reversal encrypt in Vigenere chipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -22,9 +22,10 @@ class VigenereCipheringMachine {
     this.isDirect = isDirect; // direct / reverse
   }
 
-  encrypt(message, key) {
+  encrypt(message, key, adjustKeyCb = val => val) {
     if (!message || !key) throw new Error('Incorrect arguments!');
 
+    // Should count due to non-english chars inside message which must be skipped
     let keyLetterNo = 0;
 
     const output = [...message].map(char => {
@@ -34,38 +35,22 @@ class VigenereCipheringMachine {
       const charCode = char.charCodeAt();
       const isUpperCase = this.isUpperCase(charCode);
       const baseCode = isUpperCase ? 65 : 97;
-      const keyCharCode = isUpperCase ?
-        key[keyLetterNo % key.length].toUpperCase().charCodeAt() :
-        key[keyLetterNo % key.length].toLowerCase().charCodeAt();
+
+      const keyLetter = key[keyLetterNo % key.length];
+      const keyLetterAdjusted = isUpperCase ? keyLetter.toUpperCase() : keyLetter.toLowerCase();
+      const keyCharCode = keyLetterAdjusted.charCodeAt();
 
       keyLetterNo++;
-      return String.fromCharCode((charCode + keyCharCode - 2 * baseCode) % 26 + baseCode).toUpperCase();
+      return String.fromCharCode(
+          (charCode - baseCode + adjustKeyCb(keyCharCode - baseCode)) % 26 + baseCode
+        ).toUpperCase();
     }).join('');
 
     return this.isDirect ? output : this.reverseMessage(output);
   }
 
   decrypt(message, key) {
-    if (!message || !key) throw new Error('Incorrect arguments!');
-
-    let keyLetterNo = 0;
-
-    const output = [...message].map(char => {
-      // Encrypt only English alphabet. Leave 'as is' otherwise.
-      if (!this.isEnglishLetter(char)) return char;
-
-      const charCode = char.charCodeAt();
-      const isUpperCase = this.isUpperCase(charCode);
-      const baseCode = isUpperCase ? 65 : 97;
-      const keyCharCode = isUpperCase ?
-        key[keyLetterNo % key.length].toUpperCase().charCodeAt() :
-        key[keyLetterNo % key.length].toLowerCase().charCodeAt();
-
-      keyLetterNo++;
-      return String.fromCharCode((charCode - baseCode + this.shiftKey(keyCharCode - baseCode)) % 26 + baseCode).toUpperCase();
-    }).join('');
-
-    return this.isDirect ? output : this.reverseMessage(output);
+    return this.encrypt(message, key, this.shiftKey);
   }
 
   reverseMessage(msg) {
