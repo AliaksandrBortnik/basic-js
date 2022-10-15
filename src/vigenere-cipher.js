a677e6cf6bf4df889b36bc5fbe151b8df94df629
Alex Bortnik <bortnik.ar@gmail.com>
2022-10-15 17:30:52 +0300

    Draft version of vigenere chipher encrypt/decrypt

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
@@ -20,13 +18,70 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect; // direct / reverse
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    let encrypted = '';
+    let j = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      const charCode = message[i].charCodeAt(0);
+
+      if (this.isUpperCase(charCode)) {
+        const keyCharCode = key[j % key.length].toUpperCase().charCodeAt();
+        encrypted += String.fromCharCode((charCode + keyCharCode - 2 * 65) % 26 + 65);
+        j++;
+      } else if (this.isLowerCase(charCode)) {
+        const keyCharCode = key[j % key.length].toLowerCase().charCodeAt();
+        encrypted += String.fromCharCode((charCode + keyCharCode - 2 * 97) % 26 + 97);
+        j++;
+      } else {
+        encrypted += message[i];
+      }
+    }
+
+    return encrypted.toUpperCase();
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) throw new Error('Incorrect arguments!');
+
+    let encrypted = '';
+    let j = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      const charCode = message[i].charCodeAt(0);
+
+      if (this.isUpperCase(charCode)) {
+        const keyCharCode = key[j % key.length].toUpperCase().charCodeAt();
+        encrypted += String.fromCharCode((charCode - 65 + this.shiftKey(keyCharCode - 65)) % 26 + 65);
+        j++;
+      } else if (this.isLowerCase(charCode)) {
+        const keyCharCode = key[j % key.length].toLowerCase().charCodeAt();
+        encrypted += String.fromCharCode((charCode - 97 + this.shiftKey(keyCharCode - 97)) % 26 + 97);
+        j++;
+      } else {
+        encrypted += message[i];
+      }
+    }
+
+    return encrypted.toUpperCase();
+  }
+
+  shiftKey(key) {
+    return (26 - key) % 26;
+  }
+
+  isUpperCase(char) {
+    return 65 <= char && char <= 90;
+  }
+
+  isLowerCase(char) {
+    return 97 <= char && char <= 122;
   }
 }
 
