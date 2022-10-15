8d783b20a36d6fa3852266a0d9af644059bb94a7
Alex Bortnik <bortnik.ar@gmail.com>
2022-10-15 12:40:31 +0300

    Dns stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -1,5 +1,3 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
 /**
  * Given an array of domains, return the object with the appearances of the DNS.
  *
@@ -22,9 +20,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const result = {};
+  const splittedDomains = domains.map(d => d.split('.').reverse());
+
+  for (const fullDomain of splittedDomains) {
+    for (let i = 0; i < fullDomain.length; i++) {
+      const partDomain = '.' + fullDomain.slice(0, i + 1).join('.');
+
+      result[partDomain] = result.hasOwnProperty(partDomain) ?
+        result[partDomain] + 1 :
+        1;
+    }
+  }
+
+  return result;
 }
 
 module.exports = {
